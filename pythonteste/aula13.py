bbcc756d1b724268d624c3780f8e8e309fcfd3fc
Safo001 <leopold.safo2001@hotmail.com>
2023-04-20 01:44:12 +0100

    some changes

---
+++ pythonteste/aula13.py
@@ -0,0 +1,21 @@
+"""for c in range(0,10):
+    print('Hello')
+print("FIM")"""
+
+"""for c in range(10,0,-1):
+    print(c)
+print('FIM ')"""
+
+"""i = int(input('Beginning:'))
+f = int(input('End:'))
+s = int(input('Step:'))
+
+for c in range(i,f+1,s):
+    print(c)
+print('END')"""
+
+s = 2
+for c in range (0,4):
+    n = int(input('Read a number:'))
+    s = s + n #or s += n
+print('The sum is {}.'.format(s))
